be40f27f578d987dec171bb792b583de62e398e4
shiqiancheng <13285415306@163.com>
2020-01-16 15:17:09 +0800

    提交配置

---
+++ treasure-oauth2/src/main/java/com/treasure/oauth2/Oauth2Application.java
@@ -0,0 +1,21 @@
+package com.treasure.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 简述一下～
+ * <p>
+ *
+ * @author 时前程 2020年01月16日
+ * @see
+ * @since 1.0
+ */
+@SpringBootApplication
+public class Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Application.class,args);
+    }
+}
